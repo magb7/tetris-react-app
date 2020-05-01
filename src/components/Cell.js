d52a8222046ecb3748bd9200b8b65d3f599b4216
magb7 <margaux.bedu@gmail.com>
2020-05-01 19:02:47 +0200

    remove text inside cell

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,11 +3,7 @@ import { StyledCell } from "./styles/StyledCell";
 import { TETROMINOS } from "../tetrominos";
 
 const Cell = ({ type }) => {
-  return (
-    <StyledCell type={"L"} color={TETROMINOS["L"].color}>
-      cell
-    </StyledCell>
-  );
+  return <StyledCell type={"L"} color={TETROMINOS["L"].color} />;
 };
 
 export default Cell;
