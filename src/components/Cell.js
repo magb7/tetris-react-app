6fab9d39d679d132d728a7fea7393d79143021ee
magb7 <margaux.bedu@gmail.com>
2020-05-01 18:25:29 +0200

    import style

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
 const Cell = ({ type }) => {
-  return <div>cell</div>;
+  return (
+    <StyledCell type={"L"} color={TETROMINOS["L"].color}>
+      cell
+    </StyledCell>
+  );
 };
 
 export default Cell;
